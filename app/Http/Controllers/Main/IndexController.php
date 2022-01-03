46c39a651600f606259b68849b029ea33b328a89
Alex Smirnov <alexeism1@yandex.ru>
2022-01-03 19:42:00 +0500

    first commit

---
+++ app/Http/Controllers/Main/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Main;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class indexController extends Controller
+{
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+        return view('main.index');
+    }
+}
